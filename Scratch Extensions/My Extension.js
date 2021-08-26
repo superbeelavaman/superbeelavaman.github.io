5043250b3e729919c13bded5fd42e2efc8957214
superbeelavaman <70459143+superbeelavaman@users.noreply.github.com>
2021-08-25 23:14:52 -0700

    Update My Extension.js

--- Scratch Extensions/My Extension.js
+++ Scratch Extensions/My Extension.js
@@ -12,7 +12,7 @@ class MyExtension {
       // It should never change!
       // If you choose to make an actual extension, please change this to something else.
       // Only the characters a-z and 0-9 can be used. No spaces or special characters.
-      id: 'SuperbeeExtension1',
+      id: 'superbeeextension1',
 
       // `name` is what the user sees in the toolbox
       // It can be changed without breaking projects.
