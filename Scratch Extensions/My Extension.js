0d941bc4aa70b29ff36b6e1bdd7e800a06ae048b
superbeelavaman <70459143+superbeelavaman@users.noreply.github.com>
2021-08-25 23:16:02 -0700

    Update My Extension.js

--- Scratch Extensions/My Extension.js
+++ Scratch Extensions/My Extension.js
@@ -44,7 +44,7 @@ class MyExtension {
     // You can just return a value: any string, boolean, or number will work.
     // If you have to perform an asynchronous action like a request, just return a Promise.
     // The block will wait until the Promise resolves and return the resolved value.
-    return args.VALUE1.toString() + args.VALUE2;
+    return (args.VALUE1.toString() + args.VALUE2);
   }
 }
 
