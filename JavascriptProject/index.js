035d303d6bf5e3cbc65f2e0a0ce19d43186f4850
superbeelavaman <70459143+superbeelavaman@users.noreply.github.com>
2021-11-09 15:37:26 -0800

    Update index.js

--- JavascriptProject/index.js
+++ JavascriptProject/index.js
@@ -1,1 +1,22 @@
 const canvas = document.queryselector('canvas');
+const c = canvas.getContext('2d')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+class Player {
+  cosntructor(x, y, radius, color) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+  }
+  draw() {
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true)
+    c.fill()
+  }
+}
+
+const player = new Player(100, 100, 30, 'white')
+player.draw()
