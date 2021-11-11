527f83aa5b981eb39f4f39daacc4b91258cdfb71
superbeelavaman <70459143+superbeelavaman@users.noreply.github.com>
2021-11-11 09:21:00 -0800

    Add files via upload

--- JavascriptProject/index.js
+++ JavascriptProject/index.js
@@ -1,24 +1,56 @@
 const canvas = document.querySelector('canvas')
-const ctx = canvas.getContext('2d')
+const c = canvas.getContext('2d')
 
 canvas.width = innerWidth
 canvas.height = innerHeight
 
 class Player {
-  cosntructor(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
-  }
-  draw() {
-    ctx.beginPath()
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    ctx.fillStyle = this.color
-    ctx.fill()
-  }
+    constructor(x, y, radius, color) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+    }
+    
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
 }
 
-const player = new Player(100, 100, 30, 'blue')
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+    
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+}
 
+const x = canvas.width / 2
+const y = canvas.height / 2
+
+const player = new Player(x, y, 30, 'blue')
 player.draw()
+
+addEventListener('ćlick', (event) => {
+    const projectile = new Projectile(
+        event.clientY,
+        event.clientX,
+        5,
+        'red',
+        null
+    )
+    
+    projectile.draw()
+})
