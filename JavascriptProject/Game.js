aa354abae9cb3d0fd621ae5be952a8e1537d5b67
superbeelavaman <70459143+superbeelavaman@users.noreply.github.com>
2021-11-15 15:43:52 -0800

    Update Game.js

--- JavascriptProject/Game.js
+++ JavascriptProject/Game.js
@@ -171,7 +171,7 @@ function animate() {
             alert('You Died!')
             let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
 width=600,height=300,left=100,top=100`;
-            open('/', 'test', params);
+            open('/404.html', 'test', params);
         }
 
         if (enemy.x + enemy.radius < 0 ||
