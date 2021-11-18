6bc6ce7d057bab3eedf8a8407f3d674f266851a3
superbeelavaman <70459143+superbeelavaman@users.noreply.github.com>
2021-11-18 14:00:57 -0800

    Update Game.js

--- JavascriptProject/Game.js
+++ JavascriptProject/Game.js
@@ -213,7 +213,8 @@ function animate() {
     })
     c.font = '30px DSEG14';
     c.fillStyle = 'white'
-    c.fillText(`Score: ${score}`, 10, 40)
+    c.fillText(`Score:${score}`, 10, 40)
+    c.filltext(`Try not to die!`, 10, 80)
 }
 
 addEventListener('click', (event) => {
