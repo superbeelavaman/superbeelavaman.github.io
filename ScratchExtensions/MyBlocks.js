d883c007ee1d0afc2cf3c1190c616d4f8d35cb84
superbeelavaman <70459143+superbeelavaman@users.noreply.github.com>
2022-01-14 12:25:15 -0800

    Update MyBlocks.js

--- ScratchExtensions/MyBlocks.js
+++ ScratchExtensions/MyBlocks.js
@@ -1,11 +1,23 @@
+const ArgumentType = require('../../extension-support/argument-type');
+const BlockType = require('../../extension-support/block-type');
+
 class MyBlocks {
+    constructor (runtime) {
+        /**
+         * Store this for later communication with the Scratch VM runtime.
+         * If this extension is running in a sandbox then `runtime` is an async proxy object.
+         * @type {Runtime}
+         */
+        this.runtime = runtime;
+    }
+    
     getInfo() {
         return {
             "id": "MyBlocks",
             "name": "My blocks",
             "blocks": [{
-                    "opcode": "substringy",
-                    "blockType": "reporter",
+                    "opcode": "substring",
+                    "blockType": BlockType.REPORTER,
                     "text": "letters [num1] through [num2] of [string]",
                     "arguments": {
                         "num1": {
@@ -26,8 +38,8 @@ class MyBlocks {
         "menus": { //we will get back to this in a later tutorial
         }
     };
-    substringy({num1, num2, string}) {
-        return string.substring(num1 - 1, num2);
+    substringy (args) {
+        return args.string.substring(args.num1 - 1, args.num2);
     };
 }
 Scratch.extensions.register(new MyBlocks());
