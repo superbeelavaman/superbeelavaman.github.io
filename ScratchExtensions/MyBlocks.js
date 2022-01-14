7fddf827a52bd2cf26417623abfbc11b2d9653c4
superbeelavaman <70459143+superbeelavaman@users.noreply.github.com>
2022-01-13 16:37:05 -0800

    Update MyBlocks.js

--- ScratchExtensions/MyBlocks.js
+++ ScratchExtensions/MyBlocks.js
@@ -1,3 +1,32 @@
-class MyBlocks {
+class NitroBlock {
+    getInfo() {
+        return {
+            "id": "NitroBlock",
+            "name": "NitroBlock",
+            "blocks": [{
+                    "opcode": "substringy",
+                    "blockType": "reporter",
+                    "text": "letters [num1] through [num2] of [string]",
+                    "arguments": {
+                        "num1": {
+                            "type": "number",
+                            "defaultValue": "2"
+                        },
+                        "num2": {
+                            "type": "number",
+                            "defaultValue": "5"
+                        },
+                        "string": {
+                            "type": "string",
+                            "defaultValue": "hello world"
+                        }
+                    }
+                },
+            }],
+        "menus": { //we will get back to this in a later tutorial
+        }
+    };
+    substringy({num1, num2, string}) {
+        return string.substring(num1 - 1, num2);
+    };
 }
-Scratch.extensions.register(new MyBlocks());
